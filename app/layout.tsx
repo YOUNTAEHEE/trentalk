77b746405a33dea042fd2128e13b69c8c491e66d
yth <f_hyang@naver.com>
2024-07-14 15:42:44 +0900

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,50 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import type { Metadata, Viewport } from "next";
 
+const APP_NAME = "trentalk";
+const APP_DEFAULT_TITLE = "trentalk";
+const APP_TITLE_TEMPLATE = "%s - PWA App";
+const APP_DESCRIPTION = "talk about issues";
 const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
-  title: "trentalk",
-  description: "talk about issues",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 export default function RootLayout({
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
-import withPWA from 'next-pwa';
+import withPWAInit from "@ducanh2912/next-pwa";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // 다른 설정을 여기에 추가할 수 있습니다.
-};
+const withPWA = withPWAInit({
+  dest: "public",
+});
 
 export default withPWA({
-  dest: 'public',
-  // PWA 관련 설정을 여기에 추가할 수 있습니다.
-})(nextConfig);
+  // Your Next.js config
+});
